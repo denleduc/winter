cd90f3b4464b86ee9f343d27ee1c99b9ec99a335
Denis <93516910+Bussun@users.noreply.github.com>
2023-04-24 01:26:56 +0200

    added localStorage + API key fix
    localStorage for unit, city and theme (language coming soon)

--- README.md
+++ README.md
@@ -1,27 +1,26 @@
 # Winter (I like snow, that's why)
- A simple weather app fetching its data from OpenWeatherMap's API. It has been created while following The Odin Project. Its purpose was to manipulate asynchronous JavaScript and fetch data from an API.
+ A simple weather app fetching its data from OpenWeatherMap's API. It's been created while following The Odin Project. Its purpose was to manipulate asynchronous JavaScript and fetch data from an API.
 
-!!! It doesn't work anymore, the API key is now invalid.
-
-Some info (no particular order):
+Some info (in no particular order):
 
 - It is fully responsive! (It's the first project I designed 'mobile-first')
 - You can (if you tap/click on the main temperature) see the temperature in both Celsius and Fahrenheit!
 - The background changes according to the weather
 - Simple UI
 - Dark mode
+- Preferences are stored in localStorage
 
-See it [live on my website](https://bussun.dev/projects/winter)!
+See it [live on my website](https://bussun.dev/projects/winter)! (broken link that should work again soon enough)
 
 ## Issues
 
-- I'll add credits to the resources I used asap
+- None than I can think of (feel free to open an issue if you happen to find one).
 
 ## Features to come:
 - Replacing absolute units in my CSS
 
 ## Screenshots
-(To be updated)
+(***To be updated***)
 ![PC screenshot with dark mode and celsius](./res/readme_res/screenshot.png)
 ![Mobile screenshot with light mode and fahrenheit](./res/readme_res/mobile_screenshot1.png)
 ![Mobile screenshot with dark mode and celsius](./res/readme_res/mobile_screenshot2.png)
--- weather.js
+++ weather.js
@@ -1,168 +1,278 @@
+const storage = (() => {
+  function isLocalStorageAvailable() {
+    var test = "test";
+    try {
+      localStorage.setItem(test, test);
+      localStorage.removeItem(test);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  function setTheme(value) {
+    if (isLocalStorageAvailable() === true) {
+      localStorage.setItem("theme", value);
+    }
+  }
+
+  function getTheme() {
+    if (isLocalStorageAvailable() === true) {
+      return localStorage.getItem("theme") || "light";
+    } else {
+      return "light";
+    }
+  }
+
+  function resetTheme() {
+    if (isLocalStorageAvailable() === true) {
+      localStorage.removeItem("theme");
+    }
+  }
+
+  function setCity(value) {
+    if (isLocalStorageAvailable() === true) {
+      localStorage.setItem("city", value);
+    }
+  }
+
+  function getCity() {
+    if (isLocalStorageAvailable() === true) {
+      return localStorage.getItem("city") || "Toronto";
+    } else {
+      return null;
+    }
+  }
+
+  function getUnit() {
+    if (isLocalStorageAvailable() === true) {
+      return localStorage.getItem("unit") || "C";
+    } else {
+      return "C";
+    }
+  }
+
+  function setUnit(value) {
+    if (isLocalStorageAvailable() === true) {
+      return localStorage.setItem("unit", value);
+    }
+  }
+
+  return {
+    setTheme,
+    getTheme,
+    resetTheme,
+    setCity,
+    getCity,
+    setUnit,
+    getUnit,
+    isLocalStorageAvailable,
+  };
+})();
+
 const weather = (() => {
-    const APIKey = "61d89db53ba4c888e361537f9b4b7787"; // Exposed here but it's a free api key and in this context, there's no other choice
+  const APIKey = "539bfe61f2ee16d6318e3761ac423178"; // Exposed here but it's a free api key and in this context, there's no other choice
 
-    function fetchWeather(cityName = 'Toronto') {
-        let currentWeather;
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}`, {
-            mode: 'cors'
-        })
-        .then(res => res.json())
-        .then(data => currentWeather = data)
-        .then(() => {
-            processWeather(currentWeather)
-            .then(weather => UI.updateScreen(weather));
+  function fetchWeather(cityName = storage.getCity()) {
+    let currentWeather;
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}`,
+      {
+        mode: "cors",
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => (currentWeather = data))
+      .then(() => {
+        processWeather(currentWeather).then((weather) =>
+          UI.updateScreen(weather)
+        );
+      });
+  }
+
+  function processWeather(apiInfo) {
+    return new Promise((resolve, reject) => {
+      if (apiInfo != undefined || apiInfo != null) {
+        console.log(apiInfo);
+        let ret = {
+          city: apiInfo.name,
+          weather: apiInfo.weather[0].main,
+          desc: apiInfo.weather[0].description,
+          temperature: apiInfo.main.temp,
+          feelsLike: apiInfo.main.feels_like,
+          humidity: apiInfo.main.humidity,
+          pressure: apiInfo.main.pressure,
+          minTemp: apiInfo.main.temp_min,
+          maxTemp: apiInfo.main.temp_max,
+        };
+        console.log(ret);
+        resolve(ret);
+      } else {
+        reject({
+          errMsg: "An error occured: apiInfo was equal to null or undefined",
         });
-    }
+      }
+    });
+  }
 
-    function processWeather(apiInfo) {
-        return new Promise((resolve, reject) => {
-            if (apiInfo != undefined || apiInfo != null) {
-                console.log(apiInfo);
-                let ret = {
-                    city: apiInfo.name,
-                    weather: apiInfo.weather[0].main,
-                    desc: apiInfo.weather[0].description,
-                    temperature: apiInfo.main.temp,
-                    feelsLike: apiInfo.main.feels_like,
-                    humidity: apiInfo.main.humidity,
-                    pressure: apiInfo.main.pressure,
-                    minTemp: apiInfo.main.temp_min,
-                    maxTemp: apiInfo.main.temp_max
-                };
-                console.log(ret);
-                resolve(ret);
-            }
-            else {
-                reject({errMsg: 'An error occured: apiInfo was equal to null or undefined'});
-            }
-        });
-    }
-
-    return {fetchWeather};
+  return { fetchWeather };
 })();
 
 const UI = (() => {
-    let currentTheme = 'light';
-    let currUnit = 'C';
-    let temp;
-    let minTemp;
-    let maxTemp;
-    let feelsLike;
-    const cityInput = document.querySelector('#cityInput');
-    const DOMCityName = document.querySelector('.cityName');
-    const DOMTemp = document.querySelector('.temperature');
-    const DOMFeelsLike = document.querySelector('.feelsLike');
-    const DOMMinTemp = document.querySelector('.minTemp');
-    const DOMMaxTemp = document.querySelector('.maxTemp');
-    const DOMWeather = document.querySelector('.weather');
-    const DOMHumidity = document.querySelector('.humidity');
-    const DOMPressure = document.querySelector('.pressure');
-    const toggleThemeBtn = document.querySelector('.toggleTheme');
-    const aboutBtn = document.querySelector('.about');
-    const aboutModal = document.querySelector('.aboutModal');
-    
-    cityInput.addEventListener('keydown', (e) => {
-    if (e.key == 'Enter'){
-            document.body.style.cursor = "wait";
-            getWeather(cityInput.value);
-        }
+  let currentTheme = storage.getTheme();
+  let currUnit = storage.getUnit();
+  let temp;
+  let minTemp;
+  let maxTemp;
+  let feelsLike;
+  const cityInput = document.querySelector("#cityInput");
+  const DOMCityName = document.querySelector(".cityName");
+  const DOMTemp = document.querySelector(".temperature");
+  const DOMFeelsLike = document.querySelector(".feelsLike");
+  const DOMMinTemp = document.querySelector(".minTemp");
+  const DOMMaxTemp = document.querySelector(".maxTemp");
+  const DOMWeather = document.querySelector(".weather");
+  const DOMHumidity = document.querySelector(".humidity");
+  const DOMPressure = document.querySelector(".pressure");
+  const toggleThemeBtn = document.querySelector(".toggleTheme");
+  const aboutBtn = document.querySelector(".about");
+  const aboutModal = document.querySelector(".aboutModal");
+
+  cityInput.addEventListener("keydown", (e) => {
+    if (e.key == "Enter") {
+      document.body.style.cursor = "wait";
+      getWeather(cityInput.value);
+      storage.setCity(cityInput.value);
+    }
+  });
+
+  DOMTemp.addEventListener("click", updateTemperature);
+  toggleThemeBtn.addEventListener("click", changeTheme);
+  aboutBtn.addEventListener("click", toggleAboutModal);
+
+  function getWeather(name) {
+    weather.fetchWeather(name);
+  }
+
+  function convertTemp(temp, unit) {
+    //From Kelvin to given unit: celcius or fahrenheit
+    if (unit == "C") {
+      return Number.parseFloat(temp - 273.15).toFixed(2);
+    } else {
+      return Number.parseFloat((temp - 273.15) * 1.8 + 32).toFixed(2);
+    }
+  }
+
+  function updateScreen(weatherObj) {
+    DOMCityName.innerText = weatherObj.city;
+    DOMTemp.innerText = `${convertTemp(
+      weatherObj.temperature,
+      currUnit
+    )}°${currUnit}`;
+    temp = weatherObj.temperature;
+    minTemp = weatherObj.minTemp;
+    maxTemp = weatherObj.maxTemp;
+    feelsLike = weatherObj.feelsLike;
+    DOMWeather.innerText = weatherObj.weather;
+    document.body.className = "";
+    switch (weatherObj.weather) {
+      case "Clear":
+        document.body.classList.add("clear");
+        break;
+      case "Snow":
+        document.body.classList.add("snow");
+        break;
+      case "Rain":
+        document.body.classList.add("rain");
+        break;
+      case "Drizzle":
+      case "Clouds":
+        document.body.classList.add("drizzle");
+        break;
+      case "Thunderstorm":
+        document.body.classList.add("thunderstorm");
+        break;
+      case "Mist":
+      case "Fog":
+        document.body.classList.add("mist");
+        break;
+      case "Sand":
+        document.body.classList.add("sand");
+        break;
+      case "Tornado":
+        document.body.classList.add("tornado");
+        break;
+      default:
+        break;
+    }
+    DOMMinTemp.innerText = `Min. temperature: ${convertTemp(
+      weatherObj.minTemp,
+      currUnit
+    )}°${currUnit}`;
+    DOMMaxTemp.innerText = `Max. temperature: ${convertTemp(
+      weatherObj.maxTemp,
+      currUnit
+    )}°${currUnit}`;
+    DOMFeelsLike.innerText = `Feels like: ${convertTemp(
+      weatherObj.feelsLike,
+      currUnit
+    )}°${currUnit}`;
+    DOMHumidity.innerText = `Humidity: ${weatherObj.humidity}%`;
+    DOMPressure.innerText = `Pressure: ${weatherObj.pressure} hPa`;
+    document.body.style.cursor = "default";
+  }
+
+  function updateTemperature() {
+    currUnit = currUnit == "C" ? "F" : "C";
+    storage.setUnit(currUnit);
+    DOMTemp.innerText = `${convertTemp(temp, currUnit)}°${currUnit}`;
+    DOMMinTemp.innerText = `Min. temperature: ${convertTemp(
+      minTemp,
+      currUnit
+    )}°${currUnit}`;
+    DOMMaxTemp.innerText = `Max. temperature: ${convertTemp(
+      maxTemp,
+      currUnit
+    )}°${currUnit}`;
+    DOMFeelsLike.innerText = `Feels like: ${convertTemp(
+      feelsLike,
+      currUnit
+    )}°${currUnit}`;
+  }
+
+  function setTheme() {
+    const themableElements = document.querySelectorAll(".themeable");
+    const container = document.querySelector("body");
+    if (currentTheme == "light") {
+      themableElements.forEach((el) => {
+        el.classList.remove("dark");
+        container.classList.remove("darkBody");
     });
-
-    DOMTemp.addEventListener('click', updateTemperature);
-    toggleThemeBtn.addEventListener('click', changeTheme);
-    aboutBtn.addEventListener('click', toggleAboutModal);
-
-    function getWeather(name) {
-        weather.fetchWeather(name);
+    } else {
+      themableElements.forEach((el) => {
+        el.classList.add("dark");
+        container.classList.add("darkBody");
+      });
     }
+  }
 
-    function convertTemp(temp, unit) { //From Kelvin to given unit: celcius or fahrenheit
-        if (unit == 'C') {
-            return Number.parseFloat(temp - 273.15).toFixed(2);
-        }
-        else {
-            return Number.parseFloat((temp - 273.15) * 1.8 + 32).toFixed(2);
-        }
+  function changeTheme() {
+    if (currentTheme === "light") {
+      currentTheme = "dark";
+      storage.setTheme("dark");
+    } else if (currentTheme === "dark") {
+      currentTheme = "light";
+      storage.setTheme("light");
     }
+    setTheme();
+  }
 
-    function updateScreen(weatherObj) {
-        DOMCityName.innerText = weatherObj.city;
-        DOMTemp.innerText = `${convertTemp(weatherObj.temperature, currUnit)}°${currUnit}`;
-        temp = weatherObj.temperature;
-        minTemp = weatherObj.minTemp;
-        maxTemp = weatherObj.maxTemp;
-        feelsLike = weatherObj.feelsLike;
-        DOMWeather.innerText = weatherObj.weather;
-        document.body.className = "";
-        switch (weatherObj.weather) {
-            case 'Clear':
-                document.body.classList.add('clear');
-                break;
-            case 'Snow':
-                document.body.classList.add('snow');
-                break;
-            case 'Rain':
-                document.body.classList.add('rain');
-                break;
-            case 'Drizzle':
-            case 'Clouds':
-                document.body.classList.add('drizzle');
-                break;
-            case 'Thunderstorm':
-                document.body.classList.add('thunderstorm');
-                break;
-            case 'Mist':
-            case 'Fog':
-                document.body.classList.add('mist');
-                break;
-            case 'Sand':
-                document.body.classList.add('sand');
-                break;
-            case 'Tornado':
-                document.body.classList.add('tornado');
-                break;
-            default:
-                break;
-        }
-        DOMMinTemp.innerText = `Min. temperature: ${convertTemp(weatherObj.minTemp, currUnit)}°${currUnit}`;
-        DOMMaxTemp.innerText = `Max. temperature: ${convertTemp(weatherObj.maxTemp, currUnit)}°${currUnit}`;
-        DOMFeelsLike.innerText = `Feels like: ${convertTemp(weatherObj.feelsLike, currUnit)}°${currUnit}`;
-        DOMHumidity.innerText = `Humidity: ${weatherObj.humidity}%`;
-        DOMPressure.innerText = `Pressure: ${weatherObj.pressure} hPa`;
-        document.body.style.cursor = "default";
-    }
+  function toggleAboutModal() {
+    aboutModal.classList.toggle("invisible");
+  }
 
-    function updateTemperature() {
-        currUnit = (currUnit == 'C') ? 'F' : 'C';
-        DOMTemp.innerText = `${convertTemp(temp, currUnit)}°${currUnit}`;
-        DOMMinTemp.innerText = `Min. temperature: ${convertTemp(minTemp, currUnit)}°${currUnit}`;
-        DOMMaxTemp.innerText = `Max. temperature: ${convertTemp(maxTemp, currUnit)}°${currUnit}`;
-        DOMFeelsLike.innerText = `Feels like: ${convertTemp(feelsLike, currUnit)}°${currUnit}`;
-        
-    }
-
-    function changeTheme() {
-        console.log('tapped!')
-        const themableElements = document.querySelectorAll('.themeable');
-        if (currentTheme == 'light') {
-            currentTheme = 'dark';
-            themableElements.forEach(el => {
-                el.classList.add('dark');
-            });
-        }
-        else {
-            currentTheme = 'light';
-            themableElements.forEach(el => {
-                el.classList.remove('dark');
-            });
-        }
-    }
-
-    function toggleAboutModal() {
-        aboutModal.classList.toggle('invisible');
-    }
-
-    return {updateScreen, updateTemperature, getWeather};
+  return { updateScreen, updateTemperature, getWeather, setTheme };
 })();
 
+UI.setTheme();
 UI.getWeather();
